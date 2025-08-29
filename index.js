75c60a3159a99e70017b1d0449639599a59cd493
Stas <stas@macmini.com>
2025-08-29 15:11:59 +0300

    update

--- index.js
+++ index.js
@@ -9,34 +9,38 @@ const bot = new Telegraf('8094533054:AAG7eGjNE9oPr-HZOLtqoL-3PFMUhpsq_ms')
 const localStorage = new LocalStorage('./localstorage')
 
 const requestFragment = async () => {
-    const text = await nfetch('https://fragment.com/?sort=ending').then(data => data.text())
+    try {
+        const text = await nfetch('https://fragment.com/?sort=ending').then(data => data.text())
 
-    const root = parse(text)
+        const root = parse(text)
 
-    const usernames = []
-    
-    const data = root.querySelector('tbody.tm-high-cells').childNodes.filter(data => data.querySelector)
+        const usernames = []
+        
+        const data = root.querySelector('tbody.tm-high-cells').childNodes.filter(data => data.querySelector)
 
-    for (let x = 0; x < data.length; x++) {
-        const username = data[x]
-        const isResale = !!(username.querySelector('div.table-cell-status-thin')?.innerText)
+        for (let x = 0; x < data.length; x++) {
+            const username = data[x]
+            const isResale = !!(username.querySelector('div.table-cell-status-thin')?.innerText)
 
-        if (!isResale) {
-            const name = username.querySelector('a.table-cell').getAttribute('href').replace(/\/username\//gi, '')
-                , time = username.querySelector('time').innerText
-                , ton = username.querySelector('div.icon-ton').innerText
+            if (!isResale) {
+                const name = username.querySelector('a.table-cell').getAttribute('href').replace(/\/username\//gi, '')
+                    , time = username.querySelector('time').innerText
+                    , ton = username.querySelector('div.icon-ton').innerText
 
-            const oldValue = localStorage.getItem(name)
-                , newValue = `${name}-${ton}`
+                const oldValue = localStorage.getItem(name)
+                    , newValue = `${name}-${ton}`
 
-            if (oldValue !== newValue) {
-                localStorage.setItem(name, newValue)
-                usernames.push({ name, ton, time })
+                if (oldValue !== newValue) {
+                    localStorage.setItem(name, newValue)
+                    usernames.push({ name, ton, time })
+                }
             }
         }
-    }
 
-    return usernames
+        return usernames
+    } catch (e) {
+        return []
+    }
 }
 
 const loop = async () => {
